c5ec28c6b3dd74ba4878aca0422cce4d1c697679
surya-config <a1b2c3d4config@gmail.com>
2022-05-18 01:22:33 +0530

    initial setup

---
+++ styles/breakpoints.ts
@@ -0,0 +1,19 @@
+{
+  /* 
+  mobile portrait: less than 640px (40em)
+  mobile landscape: > 640px (40em)
+  tablet portrait: > 768px (48em)
+  tablet landscape: > 1024px (64em)
+  laptop and desktop: > 1280px (80em)
+  (usually desktop will be above 1500px)
+*/
+}
+
+export const Breakpoints = {
+  xs: "0",
+  sm: "40em", // 640px
+  md: "48em", // 768px
+  lg: "64em", // 1024px
+  xl: "80em", // 1280px
+  xxl: "112.5em", // 1800px
+};
---
+++ styles/font.ts
@@ -0,0 +1,15 @@
+export const mobileFontSizes = {
+  small: "1.4rem",
+  body: "1.5rem",
+  heading: "2.1rem", // should be 1.3 times body - ~21px
+  subHeading: "1.8rem", // can be 18px or 16px but with lighter weight than the heading or body text
+  inputField: "1.6rem", // same as body text
+};
+
+export const desktopFontSizes = {
+  small: "1.6rem", // 14px
+  body: "1.8rem", // 16px
+  heading: "3.5rem", // should be around 1.96 times larger than your body text ex :- for 18px body, heading should be 35px
+  subHeading: "3.0rem", // with lighter weight than the heading or body text
+  inputField: "1.8rem", // same as body text
+};
